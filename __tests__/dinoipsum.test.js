19a6318e19e47beacd74e3e21104bbbd312be97a
Allison Sadin <aesadin@gmail.com>
2020-07-01 14:37:27 -0700

    failed second test
    co-authored-by: Teresa Rosinski <trosinski89@gmail.com>
    co-authored-by: Tyson Lackey <lackeyt90@gmail.com>

--- __tests__/dinoipsum.test.js
+++ __tests__/dinoipsum.test.js
@@ -1,13 +1,19 @@
 import { Game } from "../src/dinoipsum"
 
 describe('', () => {
+  let reusableGame;
 
+  beforeEach(() => {
+    reusableGame = new Game("string");
+  })
 
 
   test('should create class with answer property and wrong guess property', () => {
-    const game = new Game("string");
-    expect(game.answer).toBe("string");
+    expect(reusableGame.answer).toBe("string");
+  })
 
+  test('created class should have a property of hidden answer that converts string values to underscores', () => {
+    expect(reusableGame.answerHidden).toBe("______");
   })
 
 })
--- src/dinoipsum.js
+++ src/dinoipsum.js
@@ -1,6 +1,14 @@
 export class Game{
   constructor(answer){
     this.answer = answer
+    //this.answerHidden = 
+      //answer.replace(/./g, '_')
+
+
     this.guessWrong = []
+  }
+
+  checkAddGuess(){
+
   }
 }
