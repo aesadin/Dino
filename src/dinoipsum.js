d04d336454bd59b4330dfe4c2fd12481e4f9f8ff
Allison Sadin <aesadin@gmail.com>
2020-07-01 14:38:31 -0700

    passed second tst
    co-authored-by: Tyson Lackey <lackeyt90@gmail.com>
    co-authored-by: Teresa Rosinski <trosinski89@gmail.com>

--- src/dinoipsum.js
+++ src/dinoipsum.js
@@ -1,8 +1,8 @@
 export class Game{
   constructor(answer){
     this.answer = answer
-    //this.answerHidden = 
-      //answer.replace(/./g, '_')
+    this.answerHidden = 
+      answer.replace(/./g, '_')
 
 
     this.guessWrong = []
