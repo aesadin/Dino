f7bb3fa13da901ae5e599fdc547213390654d286
Allison Sadin <aesadin@gmail.com>
2020-07-01 15:38:40 -0700

    run fail test
    co-authored-by: Tyson Lackey <lackeyt90@gmail.com>
    co-authored-by: Teresa Rosinski <trosinski89@gmail.com>

--- __tests__/dinoipsum.test.js
+++ __tests__/dinoipsum.test.js
@@ -13,12 +13,17 @@ describe('', () => {
   })
 
   test('created class should have a property of hidden answer that converts string values to underscores', () => {
-    expect(reusableGame.answerHidden).toBe("______");
+    expect(reusableGame.answerHidden).toBe("_ _ _ _ _ _");
   })
 
   test('If guess exists in answer update answerHidden so that underscore at that hidden letter reveals the hidden letter', () => {
     reusableGame.checkAddGuess("r");
-    expect(reusableGame.answerHidden).toBe("__r___");
+    expect(reusableGame.answerHidden).toBe("_ _ r _ _ _");
+  })
+
+  test('If guess does not exist in answer, add guess to guessWrong array', () => {
+    reusableGame.checkAddGuess("x");
+    expect(reusableGame.guessWrong).toBe(["x"]);
   })
 
 })
--- src/dinoipsum.js
+++ src/dinoipsum.js
@@ -1,11 +1,24 @@
 export class Game{
   constructor(answer){
     this.answer = answer;
-    this.answerHidden = answer.replace(/./g, '_');
+    this.answerHidden = answer.replace(/./g, '_ ').trim();
     this.guessWrong = [];
   }
 
   checkAddGuess(guess){
-
+    if (this.answer.includes(guess)){
+      let answerArray = this.answer.split("");
+      let answerHiddenArray = [];
+      for(let i=0; i < answerArray.length; i++){
+        if (answerArray[i] === guess){
+          answerHiddenArray.push(guess);
+        } else {
+          answerHiddenArray.push("_");
+        }
+      }
+      this.answerHidden = answerHiddenArray.join(" ");
+    } else {
+      //this.guessWrong.push(guess)
+    }
   }
 }
